5ee3ca54048a79236cb4625e3f98f9ab3d0f63d1
Ali Asghar Marvi <aliasghar_marvi52@hotmail.com>
2025-03-23 16:46:57 +0100

    Reformatted files using Ruff

--- knime_extension/src/nodes/timeseries/arima_learner.py
+++ knime_extension/src/nodes/timeseries/arima_learner.py
@@ -1,5 +1,5 @@
 import logging
-import knime.extension as knext
+import knime_extension as knext
 from util import utils as kutil
 import pandas as pd
 import numpy as np
@@ -10,7 +10,6 @@ from statsmodels.tsa.stattools import kpss
 import random
 
 LOGGER = logging.getLogger(__name__)
-
 
 
 @knext.node(
@@ -53,7 +52,6 @@ class SarimaLearner:
 
     *Seasonal versions of these components operate similarly, with lag intervals equal to the seasonal period (S).*
     """
-
 
     # The parameters for the automatic ARIMA model
     max_ar = knext.IntParameter(
@@ -152,11 +150,10 @@ class SarimaLearner:
     )
 
     def configure(
-            self,
-            configure_context: knext.ConfigurationContext,
-            input_schema: knext.Schema,
-        ):
-        
+        self,
+        configure_context: knext.ConfigurationContext,
+        input_schema: knext.Schema,
+    ):
         # Checks that the given column is not None and exists in the given schema. If none is selected it returns the first column that is compatible with the provided function. If none is compatible it throws an exception.
         self.input_column = kutil.column_exists_or_preset(
             configure_context,
@@ -166,41 +163,41 @@ class SarimaLearner:
         )
 
         # dynamic predictions on log transformed column can generate invalid output
-        if (
-            self.natural_log
-            and self.dynamic_check
-        ):
+        if self.natural_log and self.dynamic_check:
             configure_context.set_warning(
                 "Enabling dynamic predictions on log transformed target column can generate invalid output."
             )
 
         # handle P > 0 and p >= s
-        if (self.min_s_ar > 0) and (
-            self.min_ar >= self.seasonal_period_param
-        ):
+        if (self.min_s_ar > 0) and (self.min_ar >= self.seasonal_period_param):
             configure_context.set_warning(
                 "Autoregressive terms overlap with seasonal autogressive terms, p should be less than S when using seasonal auto regressive terms"
             )
 
         # handle Q > 0 and q >= s
-        if (self.min_s_ma > 0) and (
-            self.min_ma >= self.seasonal_period_param
-        ):
+        if (self.min_s_ma > 0) and (self.min_ma >= self.seasonal_period_param):
             configure_context.set_warning(
                 "Moving average terms overlap with seasonal moving average terms, q should be less than S when using seasonal moving average terms."
             )
-        
-        if (self.max_ar < self.min_ar or self.max_i < self.min_i or self.max_ma < self.min_ma or 
-            self.max_s_ar < self.min_s_ar or self.max_s_i < self.min_s_i or self.max_s_ma < self.min_s_ma):
+
+        if (
+            self.max_ar < self.min_ar
+            or self.max_i < self.min_i
+            or self.max_ma < self.min_ma
+            or self.max_s_ar < self.min_s_ar
+            or self.max_s_i < self.min_s_i
+            or self.max_s_ma < self.min_s_ma
+        ):
             configure_context.set_warning(
                 "Max order parameters should be greater than or equal to min order parameters."
             )
 
-
         insamp_schema = knext.Column(knext.double(), "In-Samples")
         res_schema = knext.Column(knext.double(), "Residuals")
         model_summary_schema = knext.Column(knext.double(), "Value")
-        binary_model_schema = knext.BinaryPortObjectSpec("auto_sarima.model") # change ID for model
+        binary_model_schema = knext.BinaryPortObjectSpec(
+            "auto_sarima.model"
+        )  # change ID for model
 
         return (
             insamp_schema,
@@ -209,12 +206,7 @@ class SarimaLearner:
             binary_model_schema,
         )
 
-    def execute(
-            self,
-            exec_context: knext.ExecutionContext,
-            input: knext.Table
-        ):
-        
+    def execute(self, exec_context: knext.ExecutionContext, input: knext.Table):
         df: pd.DataFrame
         df = input.to_pandas()
         target_col: pd.Series
@@ -234,10 +226,15 @@ class SarimaLearner:
         # self.__validate(target_col)
         exec_context.set_progress(0.3)
 
-        if (self.min_ar == self.max_ar) and (self.min_i == self.max_i) and (
-            self.min_ma == self.max_ma) and (self.min_s_ar == self.max_s_ar) and (
-                self.min_s_i == self.max_s_i) and (self.min_s_ma == self.max_s_ma):
-        # model initialization and training
+        if (
+            (self.min_ar == self.max_ar)
+            and (self.min_i == self.max_i)
+            and (self.min_ma == self.max_ma)
+            and (self.min_s_ar == self.max_s_ar)
+            and (self.min_s_i == self.max_s_i)
+            and (self.min_s_ma == self.max_s_ma)
+        ):
+            # model initialization and training
             model = SARIMAX(
                 target_col,
                 order=(
@@ -257,23 +254,49 @@ class SarimaLearner:
             exec_context.set_progress(0.8)
 
         else:
-            
-            params_constraints = (self.min_ar, self.max_ar, self.min_i, self.max_i, self.min_ma, self.max_ma, self.min_s_ar, self.max_s_ar, self.min_s_i, self.max_s_i, self.min_s_ma, self.max_s_ma)
-            best_params = self.simann(target_col, self.seasonal_period_param, params_constraints, step_size=1, anneal_steps=5, mcmc_steps=10, beta0=0.1, beta1=10.0, seed=None, debug_delta_cost=False)
+            params_constraints = (
+                self.min_ar,
+                self.max_ar,
+                self.min_i,
+                self.max_i,
+                self.min_ma,
+                self.max_ma,
+                self.min_s_ar,
+                self.max_s_ar,
+                self.min_s_i,
+                self.max_s_i,
+                self.min_s_ma,
+                self.max_s_ma,
+            )
+            best_params = self.simann(
+                target_col,
+                self.seasonal_period_param,
+                params_constraints,
+                step_size=1,
+                anneal_steps=5,
+                mcmc_steps=10,
+                beta0=0.1,
+                beta1=10.0,
+                seed=None,
+                debug_delta_cost=False,
+            )
             exec_context.set_progress(0.8)
             model = SARIMAX(
-                endog = target_col,
-                order = (best_params['p'], best_params['d'], best_params['q']),
-                seasonal_order = (best_params['P'], best_params['D'], best_params['Q'], self.seasonal_period_param)
+                endog=target_col,
+                order=(best_params["p"], best_params["d"], best_params["q"]),
+                seasonal_order=(
+                    best_params["P"],
+                    best_params["D"],
+                    best_params["Q"],
+                    self.seasonal_period_param,
+                ),
             )
             trained_model = model.fit()
             exec_context.set_progress(0.9)
 
         # produce in-sample predictions for the whole series and put it in a Pandas Series
         in_samples = pd.Series(dtype=np.float64)
-        preds_col = trained_model.predict(
-            start=1, dynamic=self.dynamic_check
-        )
+        preds_col = trained_model.predict(start=1, dynamic=self.dynamic_check)
         in_samples = pd.concat([in_samples, preds_col])
         in_samples = pd.DataFrame(in_samples, columns=["In-Samples"])
 
@@ -312,11 +335,9 @@ class SarimaLearner:
                 # q
                 self.ma_order_param,
                 # S * P
-                self.seasonal_period_param
-                * self.seasonal_ar_order_param,
+                self.seasonal_period_param * self.seasonal_ar_order_param,
                 # S*Q
-                self.seasonal_period_param
-                * self.seasonal_ma_order_param,
+                self.seasonal_period_param * self.seasonal_ma_order_param,
             ]
         )
         num_of_rows = kutil.number_of_rows(column)
@@ -356,9 +377,10 @@ class SarimaLearner:
         ).rename(columns={0: "Value"})
 
         return summary
-        
-        
-    def diff_parameters(self, series, seasonality, max_i, min_i, max_is, min_is, alpha=0.05):
+
+    def diff_parameters(
+        self, series, seasonality, max_i, min_i, max_is, min_is, alpha=0.05
+    ):
         """
         Perform the KPSS test to check for stationarity in a time series.
 
@@ -372,50 +394,77 @@ class SarimaLearner:
         p_value_ds = kpss(series, regression="c", nlags="auto")[1]
 
         if p_value_ds > alpha:
-            ds_parameter = min(max(0, min_is), max_is) # Series is likely stationary at seasonal level (Fail to reject H₀)."
+            ds_parameter = min(
+                max(0, min_is), max_is
+            )  # Series is likely stationary at seasonal level (Fail to reject H₀)."
 
             # No seasonal difference applied. Now check for trend d
             p_value_d = kpss(series, regression="c", nlags="auto")[1]
-            
+
             if p_value_d > alpha:
                 print("Series is stationary at seasonal and trend levels (d=0, D=0).")
                 return (min(max(0, min_i), max_i), ds_parameter)
             else:
                 first_diff_series = series.diff().dropna()
                 p_value_d = kpss(first_diff_series, regression="c", nlags="auto")[1]
-            
+
                 if p_value_d > alpha:
-                    print("Series is stationary at seasonal level, but needs difference at trend level (d=1, D=0).")
+                    print(
+                        "Series is stationary at seasonal level, but needs difference at trend level (d=1, D=0)."
+                    )
                     return (min(max(1, min_i), max_i), ds_parameter)
                 else:
-                    print("Series is stationary at seasonal level, but needs 2 differences at trend level (d=2, D=0).")
+                    print(
+                        "Series is stationary at seasonal level, but needs 2 differences at trend level (d=2, D=0)."
+                    )
                     return (min(max(2, min_i), max_i), ds_parameter)
-        
+
         else:
-            ds_parameter = min(max(1, min_is), max_is) # Series is likely non stationary at seasonal level (Reject H₀, differencing needed)
+            ds_parameter = min(
+                max(1, min_is), max_is
+            )  # Series is likely non stationary at seasonal level (Reject H₀, differencing needed)
 
             # Apply seasonal difference
             first_seasonal_diff_series = series.diff(seasonality).dropna()
 
             # Now check for trend d after a seasonal difference has been applied
-            p_value_d = kpss(first_seasonal_diff_series, regression="c", nlags="auto")[1]
-            
+            p_value_d = kpss(first_seasonal_diff_series, regression="c", nlags="auto")[
+                1
+            ]
+
             if p_value_d > alpha:
-                print("Series is non stationary at seasonal level and stationary at trend level (d=0, D=1).")
+                print(
+                    "Series is non stationary at seasonal level and stationary at trend level (d=0, D=1)."
+                )
                 return (min(max(0, min_i), max_i), ds_parameter)
             else:
                 first_diff_series = first_seasonal_diff_series.diff().dropna()
                 p_value_d = kpss(first_diff_series, regression="c", nlags="auto")[1]
-            
+
                 if p_value_d > alpha:
-                    print("Series is non stationary at seasonal level and needs difference at trend level (d=1, D=1).")
+                    print(
+                        "Series is non stationary at seasonal level and needs difference at trend level (d=1, D=1)."
+                    )
                     return (min(max(1, min_i), max_i), ds_parameter)
                 else:
-                    print("Series is non stationary at seasonal level and needs 2 differences at trend level (d=2, D=1).")
+                    print(
+                        "Series is non stationary at seasonal level and needs 2 differences at trend level (d=2, D=1)."
+                    )
                     return (min(max(2, min_i), max_i), ds_parameter)
-                
 
-    def propose_initial_params(self, d, D, min_p=0, max_p=5, min_q=0, max_q=5, min_ps=0, max_ps=2, min_qs=0, max_qs=2):
+    def propose_initial_params(
+        self,
+        d,
+        D,
+        min_p=0,
+        max_p=5,
+        min_q=0,
+        max_q=5,
+        min_ps=0,
+        max_ps=2,
+        min_qs=0,
+        max_qs=2,
+    ):
         """
         Proposes initial random SARIMA hyperparameters.
         """
@@ -424,9 +473,21 @@ class SarimaLearner:
         P = round((min_ps + max_ps) / 2)
         Q = round((min_qs + max_qs) / 2)
 
-        return {'p': p, 'd': d, 'q': q, 'P': P, 'D': D, 'Q': Q}
+        return {"p": p, "d": d, "q": q, "P": P, "D": D, "Q": Q}
 
-    def propose_new_params(self, current_params, min_p=0, max_p=5, min_q=0, max_q=5, min_ps=0, max_ps=2, min_qs=0, max_qs=2, step_size=1):
+    def propose_new_params(
+        self,
+        current_params,
+        min_p=0,
+        max_p=5,
+        min_q=0,
+        max_q=5,
+        min_ps=0,
+        max_ps=2,
+        min_qs=0,
+        max_qs=2,
+        step_size=1,
+    ):
         """
         Proposes new random SARIMA hyperparameter(s) based on the current one with a small random step.
         """
@@ -434,27 +495,31 @@ class SarimaLearner:
         updated_params = deepcopy(current_params)
         threshold = np.random.rand()
 
-        if threshold <= (1/2): # update trend parameters (p, q)
+        if threshold <= (1 / 2):  # update trend parameters (p, q)
+            current_p, current_q = updated_params["p"], updated_params["q"]
 
-            current_p, current_q = updated_params['p'], updated_params['q']
-
-            new_p, new_q = current_p + (np.random.choice([-1, 1]) * step_size), current_q + (np.random.choice([-1, 1]) * step_size)
+            new_p, new_q = (
+                current_p + (np.random.choice([-1, 1]) * step_size),
+                current_q + (np.random.choice([-1, 1]) * step_size),
+            )
 
             new_p = min(max_q, max(min_p, new_p))
             new_q = min(max_q, max(min_q, new_q))
 
-            updated_params['p'], updated_params['q'] = new_p, new_q
-        
-        elif threshold > (1/2): # update seasonal parameters (P, Q)
+            updated_params["p"], updated_params["q"] = new_p, new_q
 
-            current_ps, current_qs = updated_params['P'], updated_params['Q']
+        elif threshold > (1 / 2):  # update seasonal parameters (P, Q)
+            current_ps, current_qs = updated_params["P"], updated_params["Q"]
 
-            new_ps, new_qs = current_ps + (np.random.choice([-1, 1]) * step_size), current_qs + (np.random.choice([-1, 1]) * step_size)
+            new_ps, new_qs = (
+                current_ps + (np.random.choice([-1, 1]) * step_size),
+                current_qs + (np.random.choice([-1, 1]) * step_size),
+            )
 
             new_ps = min(max_qs, max(min_ps, new_ps))
             new_qs = min(max_qs, max(min_qs, new_qs))
 
-            updated_params['P'], updated_params['Q'] = new_ps, new_qs
+            updated_params["P"], updated_params["Q"] = new_ps, new_qs
 
         return updated_params
 
@@ -465,10 +530,19 @@ class SarimaLearner:
         try:
             # AIC as scoring metric
             model_fit = SARIMAX(
-                endog = series,
-                order = (params['p'], params['d'], params['q']), # p, d, q order parameters for arima
-                # trend = [params['a0'], params['a1']], # A(t) = a0 + a1*t + a2*t^2 ... 
-                seasonal_order = (params['P'], params['D'], params['Q'], seasonality) # P, D, Q seasonal order parameters
+                endog=series,
+                order=(
+                    params["p"],
+                    params["d"],
+                    params["q"],
+                ),  # p, d, q order parameters for arima
+                # trend = [params['a0'], params['a1']], # A(t) = a0 + a1*t + a2*t^2 ...
+                seasonal_order=(
+                    params["P"],
+                    params["D"],
+                    params["Q"],
+                    seasonality,
+                ),  # P, D, Q seasonal order parameters
             ).fit()
 
             return model_fit.aic
@@ -490,10 +564,19 @@ class SarimaLearner:
     ## The simulated annealing generic solver.
     ## Assumes that the proposals are symmetric.
     ## NOTE: The default beta0 and beta1 are arbitrary.
-    def simann(self, series, seasonality, params_constraints, step_size=1,
-            anneal_steps = 10, mcmc_steps = 100,
-            beta0 = 0.1, beta1 = 10.0,
-            seed = None, debug_delta_cost = False):
+    def simann(
+        self,
+        series,
+        seasonality,
+        params_constraints,
+        step_size=1,
+        anneal_steps=10,
+        mcmc_steps=100,
+        beta0=0.1,
+        beta1=10.0,
+        seed=None,
+        debug_delta_cost=False,
+    ):
         ## Optionally set up the random number generator state
         if seed is not None:
             np.random.seed(seed)
@@ -508,10 +591,27 @@ class SarimaLearner:
 
         # Set up the initial configuration, compute and print the initial cost
         # unpack the parameters constraints
-        min_p, max_p, min_i, max_i, min_q, max_q, min_ps, max_ps, min_is, max_is, min_qs, max_qs = params_constraints
+        (
+            min_p,
+            max_p,
+            min_i,
+            max_i,
+            min_q,
+            max_q,
+            min_ps,
+            max_ps,
+            min_is,
+            max_is,
+            min_qs,
+            max_qs,
+        ) = params_constraints
 
-        d, D = self.diff_parameters(series, seasonality, max_i, min_i, max_is, min_is, alpha=0.05)
-        current_params = self.propose_initial_params(d, D, min_p, max_p, min_q, max_q, min_ps, max_ps, min_qs, max_qs)
+        d, D = self.diff_parameters(
+            series, seasonality, max_i, min_i, max_is, min_is, alpha=0.05
+        )
+        current_params = self.propose_initial_params(
+            d, D, min_p, max_p, min_q, max_q, min_ps, max_ps, min_qs, max_qs
+        )
 
         current_c = self.evaluate_arima_model(series, current_params, seasonality)
         print(f"initial cost = {current_c}")
@@ -529,7 +629,9 @@ class SarimaLearner:
             for t in range(mcmc_steps):
                 move = self.propose_new_params(current_params, step_size)
                 print(f"new params = {move}")
-                delta_c = self.evaluate_arima_model(series, move, seasonality) - current_c
+                delta_c = (
+                    self.evaluate_arima_model(series, move, seasonality) - current_c
+                )
                 print(f"delta_c = {delta_c}")
                 ## Optinal (expensive) check that `compute_delta_cost` works
                 # if debug_delta_cost:
