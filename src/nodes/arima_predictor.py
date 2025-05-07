7b32fe21de304e406e129c2a6582c435db19c744
Pietro Maran <pietromaran03@gmail.com>
2025-05-06 18:25:59 -0600

    Fifth commit with latest fixes

--- src/nodes/arima_predictor.py
+++ src/nodes/arima_predictor.py
@@ -2,8 +2,8 @@ import logging
 import knime.extension as knext
 from util import utils as kutil
 import numpy as np
-import pandas as pd
 import pickle
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -15,77 +15,57 @@ LOGGER = logging.getLogger(__name__)
     category=kutil.category_timeseries,
     id="auto_sarima_predictor",
 )
-# Added the input port for the time series to use to generate forecasts
-@knext.input_table(
-    name="Input Data",
-    description="Table containing data to generate forecast with the trained SARIMA model, must contain a numeric target column with no missing values to be used for forecasting.",
-)
 @knext.input_binary(
     name="Model Input",
-    description="Trained SARIMA model",
+    description="A pickled model, output from the Auto-SARIMA Learner node. This model contains all the information necessary to generate forecasts.",
     id="auto_sarima.model",
 )
 @knext.output_table(
-    name="Forecast",
-    description="Table containing forecasts for the configured column, the first value will be one timestamp ahead of the final training value used.",
+    name="Forecast periods",
+    description="Table containing the generated forecast values. The forecast starts one period after the end of the data used to train the input model.",
 )
 class SarimaForcaster:
     """
-    This node generates forecasts with a (S)ARIMA Model.
+    Generates future forecasts using a pre-trained SARIMA model, output of the node Auto-SARIMA Learner.
 
-    Based on a trained SARIMA model given at the model input port of this node, the forecasts values are computed.
+    This node takes the pre-trained SARIMA model and produces out-of-sample forecasts for a specified number of future periods.
+    The forecasts are generated directly from the end of the training data used to fit the input model.
+
+    **Key Parameters & Behavior:**
+
+    -   `Model Input` is a pickled `SARIMAXResults` object from `statsmodels`, output of node Auto-SARIMA Learner. It is the model utilized for the forecasting.
+
+    -   `Forecast periods` allows to chose the number of forecast periods, minimum is 1.
+
+    -   The quality of the forecasts heavily depends on the quality and representativeness of the input model.
+
+    -   If a log transformation was applied during training (in the Learner node), the "Reverse Log" option must be checked here to ensure forecasts are on the original scale.
+    
+    **Outputs:**
+
+    1.  Table with the forecasted values.
+
     """
 
 
     number_of_forecasts = knext.IntParameter(
         label="Forecast Periods",
-        description="Forecasts of the given time series *h* period ahead of the training data.",
+        description="Specifies the number of future time periods for which to generate forecasts. For example, if the training data ended at time T, a value of 12 here would forecast for T+1, T+2, ..., T+12.",
         default_value=1,
         min_value=1,
     )
-    dynamic_check = knext.BoolParameter(
-        label="Generate out-of-sample forecasts dynamically",
-        description="Check this box to use in-sample prediction as lagged values. Otherwise use true values.",
-        default_value=False,
-    )
     natural_log = knext.BoolParameter(
         label="Reverse Log",
-        description="Check this box if you applied the log transform inside the SARIMA Forecaster node while training your model. It will reverse the transform before generating forecasts.",
+        description="Select this option if the original data was log-transformed within the Auto-SARIMA Learner node during model training. This will apply an exponential function (np.exp) to the forecasted values to revert them to their original scale. Ensure this matches the transformation setting used in the Learner node.",
         default_value=False,
-    )
-    # target column for modelling
-    input_column = knext.ColumnParameter(
-        label="Target Column",
-        description="Table containing training data for fitting the SARIMA model, must contain a numeric target column with no missing values to be used for forecasting.",
-        port_index=0,
-        column_filter=kutil.is_numeric,
     )
 
 
     def configure(
             self,
             configure_context: knext.ConfigurationContext,
-            input_schema: knext.Schema,
             input_model
         ):
-        
-        # Checks that the given column is not None and exists in the given schema. If none is selected it returns the
-        # first column that is compatible with the provided function. If none is compatible it throws an exception.
-        self.input_column = kutil.column_exists_or_preset(
-            configure_context,
-            self.input_column,
-            input_schema,
-            kutil.is_numeric,
-        )
-
-        # dynamic predictions on log transformed column can generate invalid output
-        if (
-            self.natural_log
-            and self.dynamic_check
-        ):
-            configure_context.set_warning(
-                "Enabling dynamic predictions on log transformed target column can generate invalid output."
-            )
 
         if self.number_of_forecasts < 1:
             configure_context.set_warning(
@@ -99,43 +79,24 @@ class SarimaForcaster:
         )
 
 
-
-
     def execute(
             self, 
             exec_context: knext.ExecutionContext,
-            data_input: knext.Table,
             input_model):
 
-        df: pd.DataFrame
-        df = data_input.to_pandas()
-        target_col: pd.Series
-        target_col = df[self.input_column]
-
-        # check if log transformation is enabled
-        if self.natural_log:
-            num_negative_vals = kutil.count_negative_values(target_col)
-
-            if num_negative_vals > 0:
-                raise knext.InvalidParametersError(
-                    f" There are '{num_negative_vals}' non-positive values in the target column."
-                )
-            target_col = np.log(target_col)
         exec_context.set_progress(0.1)
 
         trained_model = pickle.loads(input_model)
-        exec_context.set_progress(0.4)
 
-        # use the method .apply to generate forecasts using the same model but with new data
-        new_trained_model = trained_model.apply(target_col)
+        exec_context.set_progress(0.5)
 
-        exec_context.set_progress(0.8)
-        
         # make out-of-sample forecasts
-        forecasts = new_trained_model.forecast(
+        forecasts = trained_model.forecast(
             steps=self.number_of_forecasts
             ).to_frame(name="Forecasts")
 
+        exec_context.set_progress(0.8)
+        
         # reverse log transformation for forecasts
         if self.natural_log:
             forecasts = np.exp(forecasts)
--- src/social_science_ext.py
+++ src/social_science_ext.py
@@ -1,3 +1,4 @@
+from nodes import arima_learner, arima_predictor  # noqa: F401
 import knime.extension as knext
 
 category = knext.category(
@@ -7,5 +8,3 @@ category = knext.category(
     description="Python Nodes for Statistical Analysis",
     icon="icons/Time_Series_Analysis.png",
 )
-
-from nodes import arima_learner, arima_predictor  # noqa: F401
