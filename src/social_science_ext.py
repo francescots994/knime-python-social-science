5450e5abf237b4da8c0744c45303d7b5d4a7353e
Pietro Maran <pietromaran03@gmail.com>
2025-05-15 23:00:23 -0600

    Changes to configuration files for bundling

--- src/social_science_ext.py
+++ src/social_science_ext.py
@@ -1,4 +1,3 @@
-from nodes import arima_learner, arima_predictor  # noqa: F401
 import knime.extension as knext
 
 category = knext.category(
@@ -8,3 +7,5 @@ category = knext.category(
     description="Python Nodes for Statistical Analysis",
     icon="icons/Time_Series_Analysis.png",
 )
+
+from nodes import arima_learner, arima_predictor  # noqa: F401
